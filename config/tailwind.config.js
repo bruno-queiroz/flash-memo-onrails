888e2cde78cc7857b68babcc47f92184e61581ab
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-24 10:09:05 -0300

    Extend tailwind colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "dark-blue": "#1b4965",
+        "primary-yellow": "#ff9f1c",
+        "aqua-blue": "#2ec4b6",
+        "light-blue": "#5fa8d3",
+      },
     },
   },
   plugins: [
