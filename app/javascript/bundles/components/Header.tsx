52fefc8b0e0d59a06af05daa291a80b0737a5784
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-24 11:23:42 -0300

    Create Header component

---
+++ app/javascript/bundles/components/Header.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { IoCloseSharp as CloseIcon } from "react-icons/io5";
+import { GiHamburgerMenu as MenuIcon } from "react-icons/gi";
+
+import { BsFillSunFill as SunIcon } from "react-icons/bs";
+import { BsFillMoonFill as MoonIcon } from "react-icons/bs";
+
+import { HeaderNavLink } from "./HeaderNavLink";
+
+export const Header = () => {
+  const [isNavActive, setIsNavActive] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const changeTheme = () => {
+    document.documentElement.classList.toggle("dark");
+    // localStorage.setItem("theme", !isDarkMode ? "dark" : "light");
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const logOut = async () => {};
+
+  return (
+    <header className="max-w-[1100px] p-4 mx-auto dark:bg-neutral-800 bg-white">
+      <div
+        className={`${
+          isNavActive ? "block" : "hidden"
+        } fixed sm:hidden top-0 bottom-0 left-0 right-0 w-full h-full bg-[rgb(0,0,0,0.7)]`}
+        onClick={() => setIsNavActive(false)}
+      />
+      <div className="flex items-center gap-4">
+        <div
+          className={`flex w-full gap-6 items-center ${
+            !true && "justify-between"
+          }`}
+        >
+          <a
+            href="/"
+            className="dark:text-white text-neutral-800 text-2xl font-bold"
+          >
+            Flash Memo
+          </a>
+
+          <nav
+            className={`${
+              isNavActive ? "translate-x-0" : "translate-x-[100%]"
+            } flex flex-col sm:flex-1 transition-transform p-4 fixed right-0 h-full w-[50%] z-10 top-0 bottom-0 dark:bg-neutral-900 bg-gray-200 sm:dark:bg-neutral-800 reset-styles`}
+          >
+            <button
+              className="text-primary-yellow text-3xl font-bold ml-auto sm:hidden"
+              onClick={() => setIsNavActive(false)}
+            >
+              <CloseIcon />
+            </button>
+            <ul className="flex flex-col sm:flex-row dark:text-white">
+              {true ? (
+                <>
+                  <li onClick={() => setIsNavActive(false)}>
+                    <HeaderNavLink name="Decks" path="/decks" />
+                  </li>
+                  <li onClick={() => setIsNavActive(false)}>
+                    <HeaderNavLink name="Add" path="/add" />
+                  </li>
+                  <li onClick={() => setIsNavActive(false)}>
+                    <HeaderNavLink name="Search" path="/search" />
+                  </li>
+                  <li
+                    onClick={() => setIsNavActive(false)}
+                    className="flex sm:flex-1 sm:justify-end p-3"
+                  >
+                    <button onClick={logOut}>
+                      {false ? "Logging out..." : "Log Out"}
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <li
+                  onClick={() => setIsNavActive(false)}
+                  className="sm:flex sm:justify-end sm:flex-1 "
+                >
+                  <HeaderNavLink name="Sign In" path="/sign-in" />
+                </li>
+              )}
+            </ul>
+          </nav>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <div className="text-dark-blue dark:text-white">
+            <MoonIcon />
+          </div>
+          <button
+            className="w-[40px] h-[23px] bg-gray-200 dark:bg-neutral-500 rounded-[100vh] p-[3px]"
+            onClick={changeTheme}
+          >
+            <div
+              className={`w-[50%] h-full transition-transform rounded-[100%] bg-aqua-blue ${
+                !isDarkMode ? "dark-theme-animation" : "light-theme-animation"
+              }`}
+            />
+          </button>
+          <div className="text-dark-blue dark:text-white">
+            <SunIcon />
+          </div>
+
+          <button
+            className="text-3xl text-dark-blue dark:text-gray-300 ml-4 sm:hidden"
+            onClick={() => setIsNavActive(true)}
+          >
+            <MenuIcon />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
