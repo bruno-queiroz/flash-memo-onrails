c2a45a5bd6a497d20d6920bcce172f231e9a159c
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-24 11:24:12 -0300

    Register Header and HeaderNavLink component

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -1,8 +1,10 @@
 import ReactOnRails from "react-on-rails";
 
-import { Test } from "../bundles/components/Test";
+import { Header } from "../bundles/components/Header";
+import { HeaderNavLink } from "../bundles/components/HeaderNavLink";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  Test,
+  Header,
+  HeaderNavLink,
 });
