6bc07b18189f2a328ca060607f127075688de626
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-25 16:21:05 -0300

    Register new components

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -2,9 +2,19 @@ import ReactOnRails from "react-on-rails";
 
 import { Header } from "../bundles/components/Header";
 import { HeaderNavLink } from "../bundles/components/HeaderNavLink";
+import { SignUp } from "../bundles/page/SignUp";
+import { Form } from "../bundles/components/Form";
+import { Input } from "../bundles/components/Input";
+import { LoadSpinner } from "../bundles/components/LoadSpinner";
+import { FormErrorMessage } from "../bundles/components/FormErrorMessage";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
+  SignUp,
+  Form,
+  FormErrorMessage,
+  Input,
+  LoadSpinner,
   Header,
   HeaderNavLink,
 });
