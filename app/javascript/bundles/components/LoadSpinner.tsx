38d2a7fade89a41c4b49878c22121cdce712885f
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-25 16:17:15 -0300

    Create LoadSpinner react component

---
+++ app/javascript/bundles/components/LoadSpinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const LoadSpinner = () => {
+  return (
+    <div className="w-[20px] mx-auto h-[20px] rounded-[50%] border-t-white border-t-[4px] bg-transparent border-l-white border-l-[4px] border-transparent border-[4px] animate-spin" />
+  );
+};
