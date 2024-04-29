cc6e5e3ebd86e5987cecb4caeceef8729d1ad721
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-29 07:58:36 -0300

    Show formatted errors

--- app/javascript/bundles/components/FormErrorMessage.tsx
+++ app/javascript/bundles/components/FormErrorMessage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FormError } from "../hooks/useSignUp";
+import { formatErrors } from "../utils/formatErrors";
 
-export const FormErrorMessage = ({
-  errorMessage,
-}: {
-  errorMessage?: string;
-}) => {
-  return <div className="text-red-500 text-center">{errorMessage}</div>;
+interface FormErrorMessageProps {
+  error: FormError;
+}
+
+export const FormErrorMessage = ({ error }: FormErrorMessageProps) => {
+  const errors = formatErrors(error);
+  return errors.map((errorMsg) => {
+    return <div className="text-red-500 text-center">{errorMsg}</div>;
+  });
 };
