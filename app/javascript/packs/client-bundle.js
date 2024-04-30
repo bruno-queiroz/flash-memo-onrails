b16fc45e70ca7790f2daa8279c101d46e3046cfb
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-30 19:17:11 -0300

    Add SignIn to client bundle

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -4,16 +4,17 @@ import { Header } from "../bundles/components/Header";
 import { HeaderNavLink } from "../bundles/components/HeaderNavLink";
 import { SignUp } from "../bundles/page/SignUp";
 import { Form } from "../bundles/components/Form";
-import { Input } from "../bundles/components/Input";
+// import { Input } from "../bundles/components/Input";
 import { LoadSpinner } from "../bundles/components/LoadSpinner";
 import { FormErrorMessage } from "../bundles/components/FormErrorMessage";
+import { SignIn } from "../bundles/page/SignIn";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   SignUp,
+  SignIn,
   Form,
   FormErrorMessage,
-  Input,
   LoadSpinner,
   Header,
   HeaderNavLink,
