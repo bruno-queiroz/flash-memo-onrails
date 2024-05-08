c18a358debe8b5850f8cbad9cfc82c4b12f05d0a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-08 16:33:54 -0300

    Remove some components

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -1,21 +1,11 @@
 import ReactOnRails from "react-on-rails";
 
 import { Header } from "../bundles/components/Header";
-import { HeaderNavLink } from "../bundles/components/HeaderNavLink";
 import { SignUp } from "../bundles/page/SignUp";
-import { Form } from "../bundles/components/Form";
-// import { Input } from "../bundles/components/Input";
-import { LoadSpinner } from "../bundles/components/LoadSpinner";
-import { FormErrorMessage } from "../bundles/components/FormErrorMessage";
 import { SignIn } from "../bundles/page/SignIn";
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   SignUp,
   SignIn,
-  Form,
-  FormErrorMessage,
-  LoadSpinner,
   Header,
-  HeaderNavLink,
 });
