f5a04d1342fce92223178c7e5ce0866c4bf5f9aa
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-09 11:24:15 -0300

    Edit devise default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  root "application#index"
+  root 'application#index'
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'sign-in', to: 'devise/sessions#new'
+    get 'sign-up', to: 'devise/registrations#new'
+    post 'sign-in', to: 'devise/sessions#create'
+    post 'sign-up', to: 'devise/registrations#create'
+    delete 'sign-out', to: 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
