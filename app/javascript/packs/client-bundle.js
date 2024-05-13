76c247d14905965605de296a94b5b6998d457bdb
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-13 07:47:33 -0300

    Register App on react on rails

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -3,9 +3,11 @@ import ReactOnRails from "react-on-rails";
 import { Header } from "../bundles/components/Header";
 import { SignUp } from "../bundles/pages/SignUp";
 import { SignIn } from "../bundles/pages/SignIn";
+import { App } from "../bundles/App";
 
 ReactOnRails.register({
   SignUp,
   SignIn,
   Header,
+  App,
 });
