91ee879693290e9347b0a85c0e15c078bf27d87a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-13 11:46:12 -0300

    Create decks controller

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,18 @@
+class DecksController < ApplicationController
+    def create
+        params[:deck][:user_id] = current_user.id
+        @deck = Deck.new(deck_params)
+
+        if @deck.save
+            render json: {isOk: true, msg: "Deck created successfully."}, status: :created
+        else
+            render json: {isOk: false, msg: "Something went wrong creating deck."}, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+        def deck_params
+            params.require(:deck).permit(:title, :user_id)
+        end
+end
