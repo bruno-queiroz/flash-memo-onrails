5ee979889138a9830b4a1c86e0eae35d42353017
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-13 12:06:41 -0300

    Send wildcard segment to application#spa

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'application#index'
+
   devise_for :users, skip: :all
   devise_scope :user do
     get 'sign-in', to: 'devise/sessions#new'
@@ -10,14 +11,8 @@ Rails.application.routes.draw do
   end
 
   post 'decks' => 'decks#create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  get '/*all' => 'decks#index'
+  get '/*spa' => 'application#spa'
 end
