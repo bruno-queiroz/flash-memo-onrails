384505f44f80edaeeeb061447b1f08a5d17e5de9
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-14 10:46:01 -0300

    Add notification reducer to store

--- app/javascript/bundles/global-state/store.ts
+++ app/javascript/bundles/global-state/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import deckReducer from "./reducers/deckSlice";
 import selectedDeckReducer from "./reducers/selectedDeckSlice";
+import notificationReducer from "./reducers/notificationSlice";
 
 export const store = configureStore({
   reducer: {
     deck: deckReducer,
     selectedDeck: selectedDeckReducer,
+    notification: notificationReducer,
   },
 });
 
