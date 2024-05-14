76e15a7d4d36c815108d7fdea3842b57ffb2253e
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-14 07:26:35 -0300

    Create spa action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
     respond_to :html, :json
     protect_from_forgery with: :null_session
     skip_before_action :verify_authenticity_token
+
+    def spa; end
 end
