70b56d1406565aa92183a131d22e03e4f0ebdb02
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-15 10:02:13 -0300

    Add useDecks custom hook

--- app/javascript/bundles/pages/Decks.tsx
+++ app/javascript/bundles/pages/Decks.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-import { useQuery } from "@tanstack/react-query";
-import { getDecks } from "../fetch/getDecks";
-
-// import { DeckStatus } from "../components/DeckStatus";
+import { DeckStatus } from "../components/DeckStatus";
 import { CreateDeckModal } from "../components/CreateDeckModal";
 import { DeleteDeckModal } from "../components/DeleteDeckModal";
 import { EditDeckNameModal } from "../components/EditDeckNameModal";
@@ -11,17 +8,10 @@ import { DeckSkeleton } from "../components/DeckSkeleton";
 import { DeckMessage } from "../components/DeckMessage";
 import { CRUDNotification } from "../components/CRUDNotification";
 
-import { useAppDispatch } from "../hooks/useAppDispatch";
-import { setIsCreateDeckModalOpen } from "../global-state/reducers/deckSlice";
+import { useDecks } from "../hooks/useDecks";
 
 export const Decks = () => {
-  const {
-    data: decks,
-    isLoading,
-    isError,
-  } = useQuery({ queryKey: ["decksData"], queryFn: getDecks });
-
-  const dispatch = useAppDispatch();
+  const { decks, isError, isPending, openCreateDeckModal } = useDecks();
 
   return (
     <section className="flex flex-col gap-6 items-center p-4 dark:text-gray-300 min-h-screen">
@@ -34,7 +24,7 @@ export const Decks = () => {
       </h1>
 
       <div className="flex flex-col gap-2 max-w-[1000px] w-full">
-        {isLoading ? (
+        {isPending ? (
           <DeckSkeleton />
         ) : (
           <>
@@ -43,8 +33,7 @@ export const Decks = () => {
             ) : isError ? (
               <DeckMessage message="Something went wrong" />
             ) : (
-              //   decks?.data?.map((deck) => <DeckStatus {...deck} key={deck.id} />)
-              <div>deck status</div>
+              decks?.data?.map((deck) => <DeckStatus {...deck} key={deck.id} />)
             )}
           </>
         )}
@@ -52,9 +41,9 @@ export const Decks = () => {
 
       <button
         type="button"
-        disabled={isLoading}
+        disabled={isPending}
         className="bg-primary-yellow py-2 px-4 rounded w-[max-content] mx-auto mt-4 text-white font-semibold"
-        onClick={() => dispatch(setIsCreateDeckModalOpen(true))}
+        onClick={openCreateDeckModal}
       >
         Create Deck
       </button>
