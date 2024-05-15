843b40be5b888872ba13431727b9d604c0e549ad
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-15 15:45:47 -0300

    Add selectedCardReducer to store

--- app/javascript/bundles/global-state/store.ts
+++ app/javascript/bundles/global-state/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import deckReducer from "./reducers/deckSlice";
 import selectedDeckReducer from "./reducers/selectedDeckSlice";
+import selectedCardReducer from "./reducers/selectedCardSlice";
 import notificationReducer from "./reducers/notificationSlice";
 
 export const store = configureStore({
   reducer: {
     deck: deckReducer,
     selectedDeck: selectedDeckReducer,
+    selectedCard: selectedCardReducer,
     notification: notificationReducer,
   },
 });
