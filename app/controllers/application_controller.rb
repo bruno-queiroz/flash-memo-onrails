1fd7dbd9e81b93c8454c8ceeafc898f1aec77801
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-15 09:20:44 -0300

    Add generic error response to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
     respond_to :html, :json
+
     protect_from_forgery with: :null_session
     skip_before_action :verify_authenticity_token
 
+    rescue_from StandardError, with: :generic_error
+
     def spa; end
+
+    private
+        def generic_error
+            render json: {isOk: false, msg: "Something went wrong."}, status: :internal_server_error
+        end
 end
