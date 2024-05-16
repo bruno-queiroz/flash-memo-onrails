c85d0a6735abb757f8ca80057e293a85acbdef0d
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-16 16:10:17 -0300

    Create useAddCard custom hook

---
+++ app/javascript/bundles/hooks/useAddCard.ts
@@ -0,0 +1,77 @@
+import React, { useRef } from "react";
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { getDecks } from "../fetch/getDecks";
+import { CreateCard, createCard } from "../fetch/createCard";
+
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { setNotification } from "../global-state/reducers/notificationSlice";
+
+export const useAddCard = () => {
+  const { data: decks } = useQuery({
+    queryKey: ["decksData"],
+    queryFn: getDecks,
+  });
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: cardMutate, isPending } = useMutation({
+    mutationFn: (newCard: CreateCard) => createCard(newCard),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["decksData"] }),
+  });
+
+  const dispatch = useAppDispatch();
+
+  const cardSelectRef = useRef<HTMLSelectElement>(null);
+  const cardFrontInputRef = useRef<HTMLTextAreaElement>(null);
+  const cardBackInputRef = useRef<HTMLTextAreaElement>(null);
+
+  const submitCard = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const defaults = {
+      ease_factor: 2.5,
+      interval: 0,
+      repetitions: 0,
+    };
+    const newCard = {
+      card: {
+        deck_id: cardSelectRef.current?.value,
+        front: cardFrontInputRef.current?.value,
+        back: cardBackInputRef.current?.value,
+        ...defaults,
+      },
+    };
+
+    try {
+      const response = await cardMutate(newCard);
+      dispatch(
+        setNotification({
+          isShowing: true,
+          isOk: true,
+          msg: response.msg,
+        })
+      );
+
+      cardFrontInputRef.current.value = "";
+      cardBackInputRef.current.value = "";
+    } catch (err) {
+      dispatch(
+        setNotification({
+          isShowing: true,
+          isOk: false,
+          msg: err,
+        })
+      );
+    }
+  };
+
+  return {
+    submitCard,
+    cardSelectRef,
+    decks,
+    cardFrontInputRef,
+    cardBackInputRef,
+    isPending,
+  };
+};
