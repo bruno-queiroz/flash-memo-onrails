5f988944a72dae483e4e408ca4a9a697bfaa40a6
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-16 16:11:20 -0300

    Add /add route to routes

--- app/javascript/bundles/routes.tsx
+++ app/javascript/bundles/routes.tsx
@@ -4,10 +4,12 @@ import { Route, Routes } from "react-router-dom";
 
 import { Decks } from "./pages/Decks";
 import { AddCard } from "./pages/AddCard";
+import { SearchCards } from "./pages/SearchCards";
 
 export default (
   <Routes>
     <Route path="/decks" element={<Decks />} />
     <Route path="/add" element={<AddCard />} />
+    <Route path="/search" element={<SearchCards />} />
   </Routes>
 );
