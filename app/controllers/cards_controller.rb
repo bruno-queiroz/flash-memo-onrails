9f61e120e0ddf838c6b3dd2c86d527343768208a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-17 08:21:31 -0300

    Create search action

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,19 @@
 class CardsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :card_not_unique
 
+    def search
+        @cards = Card.where(
+            "deck_id = :deck_id AND front LIKE :query OR back LIKE :query",
+            {
+                deck_id: params[:deck_id], query: params[:q]
+            }
+        )
+
+        if @cards.length == 0
+            render :card_not_found
+        end
+    end
+
     def create
         @card = Card.create!(card_params)
 
@@ -17,6 +30,14 @@ class CardsController < ApplicationController
                 :repetitions, 
                 :deck_id
             )
+        end
+
+        def search_cards_params
+            params.require(:card).permit(:deck_id, :query)
+        end
+
+        def card_not_found
+            render :card_not_found, status: :no_content
         end
 
         def card_not_unique
