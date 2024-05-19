6ec4acea17ff3f793624398d93ed2b937090ac27
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-19 11:32:40 -0300

    Create /study/:deckTitle route

--- app/javascript/bundles/routes.tsx
+++ app/javascript/bundles/routes.tsx
@@ -5,11 +5,13 @@ import { Route, Routes } from "react-router-dom";
 import { Decks } from "./pages/Decks";
 import { AddCard } from "./pages/AddCard";
 import { SearchCards } from "./pages/SearchCards";
+import { Study } from "./pages/Study";
 
 export default (
   <Routes>
     <Route path="/decks" element={<Decks />} />
     <Route path="/add" element={<AddCard />} />
     <Route path="/search" element={<SearchCards />} />
+    <Route path="/study/:deckTitle" element={<Study />} />
   </Routes>
 );
