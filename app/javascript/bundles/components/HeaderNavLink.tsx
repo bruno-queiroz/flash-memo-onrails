a9764687021383cd8267f658e516ddb84f1d7c41
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-22 09:57:12 -0300

    Remove styles from anchor

--- app/javascript/bundles/components/HeaderNavLink.tsx
+++ app/javascript/bundles/components/HeaderNavLink.tsx
@@ -7,12 +7,7 @@ interface HeaderNavLinkProps {
 
 export const HeaderNavLink = ({ name, path }: HeaderNavLinkProps) => {
   return (
-    <a
-      href={path}
-      className={({ isActive }) =>
-        isActive ? "dark:text-aqua-blue text-dark-blue font-semibold" : ""
-      }
-    >
+    <a href={path}>
       <div className="p-3 relative after:h-[2.5px] after:w-[0px] after:bg-dark-blue after:dark:bg-aqua-blue after:absolute after:bottom-0 after:left-0 after:transition-all after:duration-300 hover:after:w-full">
         {name}
       </div>
