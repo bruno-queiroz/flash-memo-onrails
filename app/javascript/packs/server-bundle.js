8e4abd3500d6cbb9216eb9123c7f05fcbbdabae6
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-22 10:01:19 -0300

    Register SignIn and SignUp on server-bundle

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,7 +1,11 @@
 import ReactOnRails from "react-on-rails";
 
 import { Header } from "../bundles/components/Header";
+import { SignIn } from "../bundles/pages/SignIn";
+import { SignUp } from "../bundles/pages/SignUp";
 
 ReactOnRails.register({
   Header,
+  SignIn,
+  SignUp,
 });
