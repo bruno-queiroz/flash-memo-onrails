f9e50539bad967327e4937c1afb18abe98639374
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-22 15:26:26 -0300

    Persist theme according to user preferences

--- app/javascript/bundles/hooks/useHeader.ts
+++ app/javascript/bundles/hooks/useHeader.ts
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { deleteSignOut } from "../fetch/deleteSignOut";
 
-export const useHeader = () => {
+export const useHeader = (isUserSignedIn: boolean) => {
   const [isLoggingOut, setIsLoggingOut] = useState(false);
   const [isNavActive, setIsNavActive] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   const changeTheme = () => {
     document.documentElement.classList.toggle("dark");
-    // localStorage.setItem("theme", !isDarkMode ? "dark" : "light");
+    localStorage.setItem("theme", !isDarkMode ? "dark" : "light");
     setIsDarkMode(!isDarkMode);
   };
 
@@ -17,6 +17,20 @@ export const useHeader = () => {
     await deleteSignOut();
     setIsLoggingOut(true);
   };
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+    }
+  }, []);
 
   return {
     isNavActive,
