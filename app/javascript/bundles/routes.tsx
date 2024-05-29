d62552dc941b45ace4d1f7a1daf9b48ed907615d
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-29 17:21:39 -0300

    Add Header to routes

--- app/javascript/bundles/routes.tsx
+++ app/javascript/bundles/routes.tsx
@@ -6,12 +6,16 @@ import { Decks } from "./pages/Decks";
 import { AddCard } from "./pages/AddCard";
 import { SearchCards } from "./pages/SearchCards";
 import { Study } from "./pages/Study";
+import { Header } from "./components/Header";
 
 export default (
-  <Routes>
-    <Route path="/decks" element={<Decks />} />
-    <Route path="/add" element={<AddCard />} />
-    <Route path="/search" element={<SearchCards />} />
-    <Route path="/study/:deckTitle" element={<Study />} />
-  </Routes>
+  <>
+    <Header isUserSignedIn={true} />
+    <Routes>
+      <Route path="/decks" element={<Decks />} />
+      <Route path="/add" element={<AddCard />} />
+      <Route path="/search" element={<SearchCards />} />
+      <Route path="/study/:deckTitle" element={<Study />} />
+    </Routes>
+  </>
 );
