ce8608487f514e62ff7b66254c41a7412425c10a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-30 10:19:45 -0300

    Remove Header from server-bundle

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,11 +1,9 @@
 import ReactOnRails from "react-on-rails";
 
-import { Header } from "../bundles/components/Header";
 import { SignIn } from "../bundles/pages/SignIn";
 import { SignUp } from "../bundles/pages/SignUp";
 
 ReactOnRails.register({
-  Header,
   SignIn,
   SignUp,
 });
