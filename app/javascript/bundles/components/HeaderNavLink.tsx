5e14777dcf03b684dbbf4078b1a1b1864b59a7df
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-30 10:15:56 -0300

    Change anchor tag to react router Link

--- app/javascript/bundles/components/HeaderNavLink.tsx
+++ app/javascript/bundles/components/HeaderNavLink.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
 interface HeaderNavLinkProps {
   name: string;
@@ -7,10 +8,10 @@ interface HeaderNavLinkProps {
 
 export const HeaderNavLink = ({ name, path }: HeaderNavLinkProps) => {
   return (
-    <a href={path}>
+    <Link to={path}>
       <div className="p-3 relative after:h-[2.5px] after:w-[0px] after:bg-dark-blue after:dark:bg-aqua-blue after:absolute after:bottom-0 after:left-0 after:transition-all after:duration-300 hover:after:w-full">
         {name}
       </div>
-    </a>
+    </Link>
   );
 };
