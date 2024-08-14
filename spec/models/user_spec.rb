b7098f5afb142057768ef7d48eca106eeb4ca7ee
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-08-14 16:18:30 -0300

    Create tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'User' do           
+    it 'User Should NOT create user successfully.' do   
+      user = FactoryBot.create(:user)
+
+      expect(user).to be_valid
+    end
+
+    it "User name should be unique." do 
+      user = FactoryBot.create(:user)
+
+      expect {FactoryBot.create(:user)}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "User name cannot be blank." do
+      user = FactoryBot.create(:user)
+      user.name = ""
+      expect(user).not_to be_valid
+    end
 end
