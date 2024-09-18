f9e0274e7c1e9ab80c712da2df9e914b3de23203
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-09-18 09:29:23 -0300

    Add SignUp rescript component

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -1,9 +1,10 @@
 import ReactOnRails from "react-on-rails";
 import { make as App } from "../bundles/App.res.mjs"
 import { make as SignIn } from "../bundles/pages/SignIn.res.mjs"
-
+import { make as SignUp } from "../bundles/pages/SignUp.res.mjs"
 
 ReactOnRails.register({
  App,
- SignIn
+ SignIn,
+ SignUp
 });
--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,6 +1,8 @@
 import ReactOnRails from "react-on-rails";
 import { make as SignIn } from "../bundles/pages/SignIn.res.mjs"
+import { make as SignUp } from "../bundles/pages/SignUp.res.mjs"
 
 ReactOnRails.register({
   SignIn,
+  SignUp
 });
